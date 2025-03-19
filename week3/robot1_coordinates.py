db738e2d05649d86fbdd19d6374f5bae8571a1a4
rpb <balandongiv@gmail.com>
2025-03-19 18:02:58 +0800

    first commit

---
+++ week3/robot1_coordinates.py
@@ -0,0 +1,19 @@
+robot_configuration1 = dict(
+    id="robot1",
+    center_x=327,
+    center_y=505,
+    wheel1_x=330.3,
+    wheel1_y=534.8,
+    wheel2_x=323.7,
+    wheel2_y=475.2,
+    sensor1_x=354.6,
+    sensor1_y=481.8,
+    sensor2_x=359.0,
+    sensor2_y=521.6,
+    robot_points=[
+        293.9, 478.4,
+        300.4, 538.1,
+        360.1, 531.6,
+        353.6, 471.9,
+    ]
+)
