a60477d0aeb512782de0172e8854144d0e99de93
rpb <balandongiv@gmail.com>
2025-03-24 14:11:55 +0800

    add main template for tutorial 3

---
+++ week3/week3_robot_template.py
@@ -0,0 +1,15 @@
+
+# In previous sessions, we've seen how to define variables one by one.
+
+
+## positions for robot 1 components
+center_x1 = 327
+center_y1 = 505
+wheel1_x1 = 330.3
+wheel1_y1 = 534.8
+wheel2_x1 = 323.7
+wheel2_y1 = 475.2
+sensor1_x1 = 354.6
+sensor1_y1 = 481.8
+sensor2_x1 = 359.0
+sensor2_y1 = 521.6
