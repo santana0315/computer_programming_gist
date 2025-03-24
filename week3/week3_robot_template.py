7d5cabe34036476deb1646fe71d1cf03afab02d3
rpb <balandongiv@gmail.com>
2025-03-24 15:22:02 +0800

    add structure commend to the template

--- week3/week3_robot_template.py
+++ week3/week3_robot_template.py
@@ -1,5 +1,5 @@
 
-## positions for robot 1 components
+## positions for robot 1 components and you will decoment it
 center_x1 = 327
 center_y1 = 505
 wheel1_x1 = 330.3
@@ -10,3 +10,21 @@ sensor1_x1 = 354.6
 sensor1_y1 = 481.8
 sensor2_x1 = 359.0
 sensor2_y1 = 521.6
+
+# Import the tkinter library
+
+
+# Initialize the tkinter window
+
+
+# robot configurations
+
+
+# Robot colors configuration
+
+
+
+
+# For loop to modify the robot's body color
+
+# Main tkinter loop
