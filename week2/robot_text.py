aadc784d78920d87bdb34c97e2bf7b02f3161af0
rpb <balandongiv@gmail.com>
2025-03-24 11:43:16 +0800

    refactor positoin

--- week2/robot_text.py
+++ week2/robot_text.py
@@ -1,7 +1,8 @@
 # Add text next to the robots
 text_x1 = center_x1 + 40  # Adjust the X-coordinate to position the text for robot 1
 text_y1 = center_y1
-canvas.create_text(text_x1, text_y1, text="Robot One", anchor=tk.W)
+
 text_x2 = center_x2 + 40  # Adjust the X-coordinate to position the text for robot 2
 text_y2 = center_y2
+canvas.create_text(text_x1, text_y1, text="Robot One", anchor=tk.W)
 canvas.create_text(text_x2, text_y2, text="Robot Two", anchor=tk.W)
