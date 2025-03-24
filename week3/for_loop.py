a6cb4e13ae57d80cc158e8f4ba8e758866b3d37f
rpb <balandongiv@gmail.com>
2025-03-24 15:16:11 +0800

    add for loop

---
+++ week3/for_loop.py
@@ -0,0 +1,3 @@
+for robot_configuration in robot_configurations:
+    # Access and process each robot's configuration here
+    print(robot_configuration) # For now, let's just print the whole dictionary
