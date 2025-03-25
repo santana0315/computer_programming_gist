399c5b93f782fd8ad85ede795829f420262b144b
rpb <balandongiv@gmail.com>
2025-03-25 14:31:54 +0800

    week 5 robot template

--- week6/week5_robot_template.py
+++ week6/week5_robot_template.py
@@ -13,36 +13,22 @@ class Bot:
         self.light_color = color["light_color"]
         self.body_color = color["body_color"]
 
+        # TASK 1: Complete the Position and Shape Configuration .
         # Below is the incomplete version for the robot configuration.
         # You can complete the rest of the configuration by referring to the robot_data.json file.
 
-        # self.center_x = robot_configuration["center_x"]
-        # self.center_y = robot_configuration["center_y"]
-        # self.wheel1_x = robot_configuration["wheel1_x"]
-        # self.wheel1_y = robot_configuration["wheel1_y"]
-        # self.wheel2_x = robot_configuration["wheel2_x"]
-        # self.wheel2_y = robot_configuration["wheel2_y"]
-        # self.sensor1_x = robot_configuration["sensor1_x"]
-        # self.sensor1_y = robot_configuration["sensor1_y"]
-        # self.sensor2_x = robot_configuration["sensor2_x"]
-        # self.sensor2_y = robot_configuration["sensor2_y"]
-        # self.label = robot_configuration["label"]
-        # self.robot_points = robot_configuration["robot_points"]
-
-        self.canvas = canvasp
 
 
-
-        # self.canvas = canvasp
+    # TASK 3a: Implement the draw robot Method
     def draw_robot(self):
         pass
         # canvas.create_polygon(points, fill="blue", tags=self.name)
-        ## and all the other stuff you want to do with the canvas
 
-# def registerBot(name,canvas):
-#     bot = Bot(name,canvas)
-#     canvas.create_polygon(points, fill="blue", tags=name)
-#     return bot
+
+    # TASK 2b: Adding the str Method to the Bot Class
+    # def str(self):
+
+
 
 def setup_application():
 
@@ -61,17 +47,25 @@ def load_robot_data(filepath):
 
 def main():
     root, canvas = setup_application()
-    data = load_robot_data('../robot_data.json')
+    data = load_robot_data('robot_data.json')
 
     robot_one = Bot(canvas,
                     color=data['colors']['robot1'],
                     robot_configuration=data['robot_configurations'][0])
-    print(robot_one) # Ask student to print the value for light_color
+
+    # TASK 2a: Adding the str Method to the Bot Class
+    # print(robot_one)
+
+    # TASK 3b: Implement the draw robot Method
+    # robot_one.draw_robot()
 
     # print(robot_one.light_color)
+
+    # TASK 4a: Create a Second Robot Object - robot two
     # bot2 = registerBot("bot2",canvas)
-    # bot1.draw(canvas)
     # bot2.draw(canvas)
+
+    # TASK 3c: Implement the draw robot Method
     # root.mainloop()
 
 if __name__ == "__main__":
